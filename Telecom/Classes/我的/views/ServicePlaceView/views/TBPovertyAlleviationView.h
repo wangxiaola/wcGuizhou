3e6eef9070fdb72ea5fa2225e8daec72488bcc0e
wangxiaola <1015559188@qq.com>
2017-10-16 15:10:42 +0800

    000

---
+++ Telecom/Classes/我的/views/ServicePlaceView/views/TBPovertyAlleviationView.h
@@ -0,0 +1,16 @@
+//
+//  TBPovertyAlleviationView.h
+//  Telecom
+//
+//  Created by 王小腊 on 2017/5/12.
+//  Copyright © 2017年 王小腊. All rights reserved.
+//
+
+#import "TBServicePlaceBaseView.h"
+
+/**
+ 精准扶贫
+ */
+@interface TBPovertyAlleviationView : TBServicePlaceBaseView
+
+@end
